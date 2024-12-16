eb32eb5afb7e43f1013b12879b39732e9509e560
ehan03 <eugeneh442@gmail.com>
2024-12-16 17:16:01 -0500

    checkpoint

--- src/scraping/scrapy_ufc/items.py
+++ src/scraping/scrapy_ufc/items.py
@@ -1,12 +1,6 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+# standard library imports
 
-import scrapy
+# local imports
 
-
-class ScrapyUfcItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# third party imports
+from scrapy import Field, Item
