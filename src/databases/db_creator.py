8df3b390fa9ef22edd5a7e7f0e4759d34efb74ca
ehan03 <eugeneh442@gmail.com>
2025-01-19 15:51:47 -0500

    ufcstats and wikipedia cleaned

--- src/databases/db_creator.py
+++ src/databases/db_creator.py
@@ -5,3 +5,22 @@ import sqlite3
 # third party imports
 
 # local imports
+
+
+class DatabaseCreator:
+    def __init__(self) -> None:
+        self.db_path = os.path.join(
+            os.path.dirname(__file__), "..", "..", "data", "ufc.db"
+        )
+        self.schemas_dir = os.path.join(os.path.dirname(__file__), "schemas")
+
+    def create_tables(self):
+        conn = sqlite3.connect(self.db_path)
+
+        for schema_file in os.listdir(self.schemas_dir):
+            with open(os.path.join(self.schemas_dir, schema_file), "r") as file:
+                schema = file.read()
+                conn.executescript(schema)
+
+        conn.commit()
+        conn.close()
