c456ff6e553937e286a97fd39b2e19f73156e088
ehan03 <eugeneh442@gmail.com>
2025-01-29 02:29:14 -0500

    #2 - bestfightodds cleaning done WOOHOO FREEDOM

--- src/databases/schemas/bestfightodds_schema.sql
+++ src/databases/schemas/bestfightodds_schema.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS bestfightodds_event_proposition_odds (
     betsite TEXT,
     odds INTEGER
 );
+CREATE TABLE IF NOT EXISTS bestfightodds_bout_proposition_odds (
+    tapology_bout_id TEXT,
+    event_id INTEGER,
+    fighter_id INTEGER,
+    description TEXT,
+    is_not INTEGER,
+    betsite TEXT,
+    odds INTEGER
+);
